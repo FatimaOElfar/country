6a401a746959c732ac12c585c149d5288c48be2a
Fatma Omar <105023261+FatimaOElfar@users.noreply.github.com>
2023-08-09 23:20:50 +0300

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,6 @@ import Navbar from "../component/navbar/Navbar";
 import Header from "../component/header/Header";
 import { useState, useEffect, CSSProperties } from "react";
 import Cart from "../component/cart/Cart";
-import Loader from "../component/Loader";
-import { PulseLoader } from "react-spinners/PulseLoader";
-// import "react-spinners/PulseLoader.css";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
